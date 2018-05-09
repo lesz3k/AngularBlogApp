39f492009c3918c9ed3b083152b91fe0c6c26a41
leszkasp <leszek.kasperczyk@pearson.com>
2018-05-09 11:20:36 +0200

    added mongoose, bodyparser; added router link

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const url = 'mongodb://localhost/blogTestDb'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
