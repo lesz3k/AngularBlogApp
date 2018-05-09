d282bbe7d7bb71d478fdb2a761f7a22f8bdde1e2
leszkasp <leszek.kasperczyk@pearson.com>
2018-05-09 10:48:48 +0200

    added root component, changed the index.html, added root module to the app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +14,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
---
+++ client/src/app/root/root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['root.component.css'],
+})
+export class RootComponent {
+
+}
