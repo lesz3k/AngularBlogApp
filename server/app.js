ca2a0f169fba996a9c8d22c399d0f6fd3cf9a3bf
leszkasp <leszek.kasperczyk@pearson.com>
2018-05-09 12:20:51 +0200

    added function to add 3 posts if the database is empty

--- server/app.js
+++ server/app.js
@@ -5,6 +5,52 @@ const bodyParser = require('body-parser')
 const url = 'mongodb://localhost/blogTestDb'
 const Post = require('./model/post')
 
+mongoose.connect(url)
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise
+//Get the default connection
+const db = mongoose.connection,
+currentDate = new Date().toLocaleString();
+
+db.once('open', function() {
+  console.log('connected to DB!')
+
+const newPostsArr = [{ title : 'First post from MongoDB',
+                        description : 'This is first blog entry from MongoDB',
+                        tags: ['tech', 'science', 'travel', 'fashion', 'finance', 'business', 'IT'],
+                        date: currentDate
+                        },
+                        { title : 'Fashion & Travel post',
+                          description : 'Fashion and travel',
+                          tags: ['travel', 'fashion'],
+                          date: currentDate
+                        },
+                        { title : 'Tech & Science post',
+                          description : 'Tech and science',
+                          tags: ['tech', 'science'],
+                          date: currentDate
+                        }
+  ]
+    //add first posts if posts db is empty
+    Post.find(function (err, post) {
+      if (err) return console.error(err);
+      if(post.length===0){
+        /*newPost.save(function (err, post) {
+          if (err) return console.error(err);
+          console.log(post.title + " saved to posts collection.");
+        });*/
+        for (let item of newPostsArr) {
+          let newPost = new Post(item)
+          newPost.save(function (err, post) {
+            if (err) return console.error(err);
+            console.log(post.title + " saved to posts collection.");
+          });
+        }
+      }
+    })
+
+});
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false}))
 
